4d7d5613a87cb8658e0ec984e0ad7df9c9fe5088
MEGHNA <161241145+MeghnaaT@users.noreply.github.com>
2026-02-22 01:55:10 +0530

    Refactor missing number function into class method
    Refactor function to find missing number in a class structure with improved clarity and comments.

--- find_missing_number.py
+++ find_missing_number.py
@@ -1,5 +1,19 @@
 # Find the Missing Number: Find the missing number in an array of size n containing numbers from 1 to n.
 
-def find_missing_number(self, arr):
-    n= len(arr)+1
-    return n*(n+1)//2 - sum(arr)
+class Solution(object):
+    def missingNumber(self, nums):
+        n = len(nums)
+        expected = n*(n+1)//2
+        actual = sum(nums)
+        return expected-actual
+# TC: O(n)
+# SC: O(1)
+
+# Example Walkthrough
+
+#Input: nums = [3,0,1]
+# n = 3
+# Expected sum = 3 * 4 / 2 = 6
+# Actual sum = 3 + 0 + 1 = 4
+# Missing = 6 - 4 = 2
+# Output: 2
