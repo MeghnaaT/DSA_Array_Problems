84639e8ee10ddc0d8ba830bcd830e2ae3573e208
MEGHNA <161241145+MeghnaaT@users.noreply.github.com>
2026-02-22 01:06:44 +0530

    Add merge function for two sorted arrays
    Implemented a three-pointer method to merge two sorted arrays in-place.

--- merge_two_sorted_arrays.py
+++ merge_two_sorted_arrays.py
@@ -1,4 +1,27 @@
 # Merge Two Sorted Arrays
 
-def merge_sorted(self,a ,b):
-    return sorted(a+b)
+# 3 pointer method
+
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        p1, p2, p = m - 1, n - 1, m + n - 1
+        # p1 : last valid element in nums1
+        # p2: last valid element in nums2
+        # p: last position in nums1(where merged result goes)
+        
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+        
+        # If nums2 still has elements left
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
+# TC: O(m+n)
+# SC:O(1)
