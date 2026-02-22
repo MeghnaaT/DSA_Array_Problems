ab6410c1367596dcaa559b55f79fbe789af9022e
MEGHNA <161241145+MeghnaaT@users.noreply.github.com>
2026-02-22 13:04:40 +0530

    Refactor majority element function to class method

--- Majority Element.py
+++ Majority Element.py
@@ -1,10 +1,19 @@
 # Find Majority Element: Find the element that appears more than n/2 times.
 
-def majority_element(arr):
-    count = 0
-    candidate = None
-    for num in arr:
-        if count == 0:
-            candidate = num
-        count += (1 if num == candidate else -1)
-    return candidate
+# Boyer - Moore Voting Algorithm
+
+class Solution:
+    def majorityElement(self, nums):
+        # Initialize candidate and counter
+        candidate = None
+        count = 0
+        
+        # Voting process
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+        
+        # Since the problem guarantees majority element exists,
+        # candidate will be the answer
+        return candidate
